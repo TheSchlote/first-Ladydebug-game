42e850aeb27607fbafc1e66384d9b8495497a10b
TheSchlote <TheSchlote@gmail.com>
2021-02-24 22:20:22 -0600

    Added Logans background
    Tried to add logic for the cpu and coffee to dissapear but the spawning back isnt quite right.

--- Ladydebug game/Assets/Scripts/BackgroundScroller.cs
+++ Ladydebug game/Assets/Scripts/BackgroundScroller.cs
@@ -2,12 +2,19 @@
 
 public class BackgroundScroller : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject coffeePrefab;
+    [SerializeField]
+    private GameObject microchipPrefab;
+
+    private GameObject coffee;
+    private GameObject microchip;
+
     private BoxCollider2D collider;
     private Rigidbody2D rb;
 
     private float height;
     private float scrollSpeed = -2f;
-
 
     void Start()
     {
@@ -18,22 +25,54 @@ public class BackgroundScroller : MonoBehaviour
         collider.enabled = false;
 
         rb.velocity = new Vector2(0, scrollSpeed);
-        ResetObstacle();
+
+        coffee = Instantiate(coffeePrefab, new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)), Quaternion.identity);
+        coffee.transform.parent = transform.GetChild(0);
+        Destroy(coffee);
+
+        microchip = Instantiate(microchipPrefab, new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)), Quaternion.identity);
+        microchip.transform.parent = transform.GetChild(0);
+        Destroy(microchip);
+
+        ResetObstacle(0);
     }
 
     void Update()
     {
-        if (transform.position.y < -height)
+        if (transform.GetChild(0).position.y < -height)
         {
             Vector2 resetPosition = new Vector2(0, height * 2f);
-            transform.position = (Vector2)transform.position + resetPosition;
-            ResetObstacle();
+            transform.GetChild(0).position = (Vector2)transform.GetChild(0).position + resetPosition;
+            ResetObstacle(0);
+        }
+
+        if (transform.GetChild(1).position.y < -height)
+        {
+            Vector2 resetPosition = new Vector2(0, height * 2f);
+            transform.GetChild(1).position = (Vector2)transform.GetChild(1).position + resetPosition;
+            ResetObstacle(1);
         }
     }
-
-    private void ResetObstacle()
+    
+    public void ResetObstacle(int child)
     {
-        transform.GetChild(0).localPosition = new Vector2(Random.Range(-4, 4), Random.Range(-4, 4));
-        transform.GetChild(1).localPosition = new Vector2(Random.Range(-4, 4), Random.Range(-4, 4));
+        if (!coffee)
+        {
+            coffee = Instantiate(coffeePrefab, new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)), Quaternion.identity);
+            coffee.transform.parent = transform.GetChild(child);
+        }
+        coffee.transform.localPosition = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
+
+        if (!microchip)
+        {
+            microchip = Instantiate(microchipPrefab, new Vector3(Random.Range(-1, 1), Random.Range(-1, 1)), Quaternion.identity);
+            microchip.transform.parent = transform.GetChild(child);
+        }
+        microchip.transform.localPosition = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
+    }
+
+    public void DestroyCoffee()
+    {
+        Destroy(coffee);
     }
 }
--- Ladydebug game/Assets/Scripts/Ladybug.cs
+++ Ladydebug game/Assets/Scripts/Ladybug.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ladybug : MonoBehaviour
 {
+    public BackgroundScroller background;
+
+    [SerializeField] private Slider coffeeMeter;
     private Rigidbody2D rb;
     private float moveSpeed = 5;
     private void Start()
     {
+        coffeeMeter.maxValue = 50;
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
@@ -21,7 +26,9 @@ public class Ladybug : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Coffee"))
         {
+            background.DestroyCoffee();
             moveSpeed += 1;
+            coffeeMeter.value = moveSpeed;
             Debug.Log("Drank Coffee! Go Faster!");
         }
 
